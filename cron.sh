4804fbd1b10d51f50b40232e48b3b0e5001a7515
Andy Reagan <github@andyreagan.com>
2018-08-22 17:18:41 -0400

    Pass 2 submit script written and ran.

--- cron.sh
+++ cron.sh
@@ -1,4 +1,5 @@
 cd /users/a/r/areagan/scratch/2015-11-ambient-bonanza
-/gpfs1/arch/x86_64/python2.7.5/bin/python qsub.py
+# /gpfs1/arch/x86_64/python2.7.5/bin/python qsub.py
+/gpfs1/arch/x86_64/python2.7.5/bin/python qsub_pass2.py
 # grep -l "delete me" keywordScrape.o* | xargs rm
 mv keywordScrape.o* output
---
+++ qsub_pass2.py
@@ -0,0 +1,49 @@
+# accept a timestamp
+# read the current date
+# pump out a qsub script named by the timestamp, for the current date
+
+import datetime
+import sys
+import subprocess
+import os
+import time
+
+jobs = int(subprocess.check_output("showq | grep areagan | wc -l",shell=True))
+print(jobs)
+
+max_jobs = 500
+# max_jobs = 150
+jobs_remaining = max_jobs - jobs
+print(jobs_remaining)
+
+loop_counter = 0
+
+batch_size = 1
+
+end = datetime.datetime(2018,7,24)
+start = datetime.datetime(2008,9,13)
+curr = start
+dt = datetime.timedelta(minutes=15)
+while curr < end and jobs_remaining > batch_size:
+    f = curr.strftime("%Y-%m-%d/%Y-%m-%d-%H-%M")
+    if not os.path.isfile("keywords/"+f+".pkl"):
+        job='''#PBS -l nodes=1:ppn=1
+#PBS -l walltime=30:00:00
+#PBS -N keywordScrape
+#PBS -j oe
+
+cd /users/a/r/areagan/scratch/2015-11-ambient-bonanza
+
+echo "processing {0}"
+python3=/users/a/r/areagan/scratch/realtime-parsing/RHEL7-python-3.5.1/bin/python
+/usr/bin/time -v gzip -cd /users/c/d/cdanfort/scratch/twitter/tweet-troll/zipped-raw/{0}.gz | $python3 processTweets.py "keywords/{0}.pkl"
+
+echo "delete me"'''.format(f)
+
+        # subprocess.call("echo '{0}' | qsub -qshortq".format(job),shell=True)
+        subprocess.call("echo '{0}' | qsub".format(job),shell=True)
+        time.sleep(0.1)
+        
+        jobs_remaining -= batch_size
+        print("jobs submitted, {0} jobs remaining".format(jobs_remaining))
+    curr += dt
